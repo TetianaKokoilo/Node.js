26804717f586b2488542bafe77e70564473d516e
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-05-02 21:43:06 +0200

    add task with json file contacts

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello");
+const contacts = require("./db/contacts");
+// console.log(contacts);
+// index.js
+// const argv = require("yargs").argv;
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+	switch (action) {
+		case "list":
+			const allContacts = await contacts.listContacts();
+			return console.log(allContacts);
+			break;
+
+		case "get":
+			const getContactById = await contacts.getContactById(id);
+			return console.log(getContactById);
+			break;
+
+		case "add":
+			const newContact = await contacts.addContact({ name, email, phone });
+			return console.log(newContact);
+			break;
+
+		case "remove":
+			const removeContact = await contacts.removeContact(id);
+			return console.log(removeContact);
+			break;
+
+		//     default:
+		//       console.warn("\x1B[31m Unknown action type!");
+	}
+}
+
+// invokeAction({ action: "get", id: "drsAJ4SHPYqZeG-83QTVW" });
+// invokeAction({ action: "add", name: "Tetiana Kokoilo", email: "kokoiloteitana@gmail.com", phone: "(098) 456-9977" });
+// invokeAction({ action: "remove", id: "YGV5ZEpjOS5coNKNUfnrb"});
